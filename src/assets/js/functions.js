a9bb9fb9fb4b50aeb3fc0c32d385e69f1cd70253
Martienskie <maarten.de.meyere@student.howest.be>
2020-03-19 15:27:41 +0100

    add function to create game, show all games, go to another page

--- src/assets/js/functions.js
+++ src/assets/js/functions.js
@@ -1,14 +1,33 @@
 "use strict";
 
-let joinGame = (e) => {
-    e.preventDefault();
-    let username = usernameInput.value.toLowerCase();
-    let gameID = gameIDInput.value;
+let goToPageInSecond = (page) => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(window.location.href = page);
+        }, 500);
+    });
+};
 
-    fetchFromServer(`${config.root}games/${gameID}/players`, 'POST', {playerName: `${username}`}).then(
+let createGame = (username) => {
+    let gameId = null;
+    fetchFromServer(`${config.root}games`, 'POST', {"prefix": "group27"}).then(
+        function (response) {
+            gameId = response;
+            joinGame(username, gameId);
+        });
+};
+
+let joinGame = (username, gameId) => {
+    fetchFromServer(`${config.root}games/${gameId}/players`, 'POST', {playerName: `${username}`}).then(
         function (response) {
             console.log(response);
             localStorage.setItem('playerToken', `${response}`);
-            window.location.pathname = 'webclient/src/lobbyGuest.html';
+        });
+};
+
+let showAllGames = () => {
+    fetchFromServer(`${config.root}games?details=true&prefix=group${config.groupnumber}`, 'GET').then(
+        function (response) {
+            console.log(response);
         });
 };
