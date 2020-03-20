30a9b9a8f9617fcc664f285a33136a092388094d
niels.baudewijns <niels.baudewijns@student.howest.be>
2020-03-20 15:11:26 +0100

    code mooi maken

--- src/assets/js/startGame/givePlayersStartItems.js
+++ src/assets/js/startGame/givePlayersStartItems.js
@@ -12,17 +12,14 @@ function getStartGameInfo(){
     fetchFromServer(`${config.root}games/${gameId}`, 'GET').then(
         function (response) {
             console.log(response);
-
             giveBankMoney(response);
         });
 }
 
 function giveBankMoney(response) {
     bankMoney.innerHTML = "";
-
     for (let i = 0; i < response.bank.length; i ++) {
         bankMoney.innerHTML += `<p class="${response.bank[i].currency}">${response.bank[i].amount}</p>`;
     }
-
 }
 
