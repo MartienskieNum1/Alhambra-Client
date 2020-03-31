75ba0cdd2663c06cc1f721aa3ab06d2d1d8e81a3
aron.delbol <aron.delbol@student.howest.be>
2020-03-31 13:09:37 +0200

    did the last name + changed name in functions

--- src/assets/js/startGame/myAlhambra.js
+++ src/assets/js/startGame/myAlhambra.js
@@ -4,7 +4,7 @@ let goBack = document.querySelector('.back');
 
 function init(){
     goBack.addEventListener('click', function () {
-        window.location.href = "../src/general_board.html";
+        window.location.href = "../../../generalBoard.html";
     });
     getAlhambraInfo();
 }
