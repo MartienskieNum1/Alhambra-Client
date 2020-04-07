909e207c8405656c8d766913835ba2ced3918ed1
Martienskie <maarten.de.meyere@student.howest.be>
2020-04-07 15:49:26 +0200

    make function which inserts all buildings in city

--- src/assets/js/startGame/myAlhambra.js
+++ src/assets/js/startGame/myAlhambra.js
@@ -9,32 +9,26 @@ function init(){
         window.location.href = "../src/generalBoard.html";
     });
     getAlhambraInfo();
-    makeDivs();
+    makeDivsAndListeners();
 }
 
 document.addEventListener("DOMContentLoaded", init);
 
-function makeDivs(){
+function makeDivsAndListeners() {
     let columns = 11;
-    let output = document.querySelector(".city");
-    output.innerHTML = "";
+    let city = document.querySelector(".city");
+    city.innerHTML = "";
     for (let i = 0;i < columns;i++){
         for (let j = 0;j < columns;j++){
-            if (i === Math.floor(columns/2) && j === Math.floor(columns/2)){
-                output.innerHTML += `<div class="fountain buildingInAlhambra" data-row="${Math.round(i-columns/2)}"
-                data-column="${Math.round(j-columns/2)}">
-                <img src="http://localhost:63342/webclient/src/images/Fountain.webp" alt="fountain"/></div>`;
-            }
-            else{
-                output.innerHTML += `<div class="buildingInAlhambra" data-row="${Math.round(i-columns/2)}"
-                data-column="${Math.round(j-columns/2)}"></div>`;
-            }
-            
+            city.innerHTML += `<div class="buildingInAlhambra" data-row="${Math.round(i-columns/2)}"
+            data-column="${Math.round(j-columns/2)}"></div>`;
         }
     }
+
+    insertBuildings();
     
-    let divs = document.querySelectorAll(".buildingInAlhambra");
     let location;
+    let divs = document.querySelectorAll(".buildingInAlhambra");
     divs.forEach(div => {
         div.addEventListener("click", function (e) {
             location = {
@@ -46,6 +40,50 @@ function makeDivs(){
     });
 }
 
+function insertBuildings() {
+    let username = localStorage.getItem('username');
+    let gameId = localStorage.getItem('gameId');
+    let divs = document.querySelectorAll(".buildingInAlhambra");
+    let totalRows = 0;
+    let myCity;
+    fetchFromServer(`${config.root}games/${gameId}`, 'GET').then((response) => {
+        for (let player of response.players) {
+            if (player.name === username) {
+                    myCity = player.city;
+                }
+            }
+
+        for (let row in myCity) {
+            totalRows++;
+        }
+
+        for (let rowIn in myCity) {
+            for (let colIn in myCity[rowIn]) {
+                if (myCity[rowIn][colIn]) {
+                    let building = myCity[rowIn][colIn];
+                    divs.forEach(div => {
+                        let divRow = parseInt(div.getAttribute('data-row'));
+                        let divCol = parseInt(div.getAttribute('data-column'));
+                        let buildingRow = Math.round(rowIn - totalRows / 2);
+                        let buildingCol = Math.round(colIn - totalRows / 2);
+                        if (divRow === buildingRow && divCol === buildingCol) {
+                            if (!building.type) {
+                                div.innerHTML = `<p class="fountain"></p>`;
+                            } else {
+                                div.innerHTML = `<p class="${building.type}">${building.cost}</p>`;
+                                for (let [key, value] of Object.entries(building.walls)) {
+                                    if (value) {
+                                        div.firstElementChild.classList.add(key);
+                                    }
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+        }
+    })
+}
 
 function displayTotalValue() {
     let totalValue = document.querySelector(".totalValue");
