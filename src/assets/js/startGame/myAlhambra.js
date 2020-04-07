86d6630ce316857401d648b351a22543899b35a4
Martienskie <maarten.de.meyere@student.howest.be>
2020-04-07 13:12:47 +0200

    put alhambra inside a div to give fixed size + separate scorecard and my reserve

--- src/assets/js/startGame/myAlhambra.js
+++ src/assets/js/startGame/myAlhambra.js
@@ -16,13 +16,14 @@ document.addEventListener("DOMContentLoaded", init);
 
 function makeDivs(){
     let columns = 11;
-    let output = document.querySelector("#yourAlhambra");
+    let output = document.querySelector(".city");
     output.innerHTML = "";
     for (let i = 0;i < columns;i++){
         for (let j = 0;j < columns;j++){
             if (i === Math.floor(columns/2) && j === Math.floor(columns/2)){
                 output.innerHTML += `<div class="fountain buildingInAlhambra" data-row="${Math.round(i-columns/2)}"
-                data-column="${Math.round(j-columns/2)}"><img src="http://localhost:63342/webclient/src/images/Fountain.webp"></div>`;
+                data-column="${Math.round(j-columns/2)}">
+                <img src="http://localhost:63342/webclient/src/images/Fountain.webp" alt="fountain"/></div>`;
             }
             else{
                 output.innerHTML += `<div class="buildingInAlhambra" data-row="${Math.round(i-columns/2)}"
