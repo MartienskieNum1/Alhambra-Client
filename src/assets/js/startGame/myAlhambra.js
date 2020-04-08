5e9931a9083bfb17ef050feac587fadcbdae0601
robbecordy <robbe.cordy@student.howest.be>
2020-04-08 13:17:47 +0200

    you can now see other peoples their alhambra as well

--- src/assets/js/startGame/myAlhambra.js
+++ src/assets/js/startGame/myAlhambra.js
@@ -162,6 +162,58 @@ function showThisAlhambra(e, response) {
         total += parseInt(money.textContent);
     });
 
+    let columns = 11;
+    let city = document.querySelector(".city");
+    city.innerHTML = "";
+    for (let i = 0;i < columns;i++){
+        for (let j = 0;j < columns;j++){
+            city.innerHTML += `<div class="buildingInAlhambra" data-row="${Math.round(i-columns/2)}"
+            data-column="${Math.round(j-columns/2)}"></div>`;
+        }
+    }
+
+    let gameId = localStorage.getItem('gameId');
+    let divs = document.querySelectorAll(".buildingInAlhambra");
+    let totalRows = 0;
+    let myCity;
+    fetchFromServer(`${config.root}games/${gameId}`, 'GET').then((response) => {
+        for (let player of response.players) {
+            if (player.name === username) {
+                myCity = player.city;
+            }
+        }
+
+        for (let row in myCity) {
+            totalRows++;
+        }
+
+        for (let rowIn in myCity) {
+            for (let colIn in myCity[rowIn]) {
+                if (myCity[rowIn][colIn]) {
+                    let building = myCity[rowIn][colIn];
+                    divs.forEach(div => {
+                        let divRow = parseInt(div.getAttribute('data-row'));
+                        let divCol = parseInt(div.getAttribute('data-column'));
+                        let buildingRow = Math.round(rowIn - totalRows / 2);
+                        let buildingCol = Math.round(colIn - totalRows / 2);
+                        if (divRow === buildingRow && divCol === buildingCol) {
+                            if (!building.type) {
+                                div.innerHTML = `<p class="fountain"></p>`;
+                            } else {
+                                div.innerHTML = `<p class="${building.type}">${building.cost}</p>`;
+                                for (let [key, value] of Object.entries(building.walls)) {
+                                    if (value) {
+                                        div.firstElementChild.classList.add(key);
+                                    }
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+        }
+    });
+
     totalValue.innerHTML = `Total value: ${total}`;
 
     playerMoney.innerHTML = "";
