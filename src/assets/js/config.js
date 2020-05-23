a7b531f4b6b80ea27099b2cc85291ac57a24ff85
niels.baudewijns <niels.baudewijns@student.howest.be>
2020-05-23 18:08:46 +0200

    css generalBoard

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -2,6 +2,9 @@
 // Feel free to extend this object with further configurables for your application
 
 const config = {
-    root: "http://172.21.22.52:48201/alhambra-api/",
+   // root: "http://172.21.22.52:48201/alhambra-api/",
+   //
+   // root: "http://localhost:8080/"
+    root: "http://172.21.22.52:48201/alhambra-27/api/",
     groupnumber: "27"
 };
